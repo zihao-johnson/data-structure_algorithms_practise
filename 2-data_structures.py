a893ebbd3a065903991cf3d072382387a352ed13
Zihao <johnson2811hd@gmail.com>
2020-11-21 22:14:29 +1100

    Update 2-data_structures.py

--- 2-data_structures.py
+++ 2-data_structures.py
@@ -18,6 +18,15 @@ A. Abstract Data Type (ADT) : is theoretical concepts of data structure from use
         g. insertBefore(p,e)
         h. insertAfter(p,e)
         i. remove(p)
+    
+    Conclusion:
+        all lists ADT type and their implementation could be used to structure data type (or ADT):
+            a. Stack (LIFO)
+            b. Queue (FIFO)
+        with different rules applied on behaviors of naive lists
+    
+    3. Tree:
+
 B. Data Structure : concrete representation from implementer's view
    1. Array-based Lists --> see "array class"
         python has not arrary data type without libraries imported, it has only four built-in data type (list, tuple, set and dictionaries),
@@ -27,7 +36,7 @@ B. Data Structure : concrete representation from implementer's view
    2. concrete data structure for positional list ADT:
     a. Singly Linked List --> see "linked_list class"
     b. Doubly Linked List --> see "linked_list class"   (same class but argu if_Doubly_linked indicates whether it is singly or doubly linked list)
-
+    C. Notes, if the all argus {p,e} are given as the data that stored in nodes, the functions will noe be used and all functions will be run in O(n) if pointers are not given !
 
 '''
 
@@ -106,7 +115,8 @@ class LL_node():
         # pointers is two element list if singly linked, else three elements list
         self.next_node = next_node
         self.if_Doubly_linked = if_Doubly_linked
-        if if_head==False:
+        self.if_head = if_head
+        if self.if_head==False:
             self.data = data
             if self.if_Doubly_linked == True:
                 self.previous_node = pre_node
@@ -115,29 +125,56 @@ class linked_list():
     def __init__(self, nodes, if_Doubly_linked = False):
         # nodes is a list start with header node,
         # initially will linked all nodes in order.
-        self.header = nodes[0]
-        self.nodes = nodes
-        self.if_Doubly_linked = if_Doubly_linked
-        self.size = 1  # size include header node
-        for i in range(0,len(nodes)-1):
-            nodes[i].next_node = nodes[i+1]
-            self.size += 1
-        if if_Doubly_linked == True:
-            for i in range(1,len(nodes)):
-                nodes[i].pre_node = nodes[i-1]
+        if len(nodes) == 0:
+            print("warning: list's header and elements does not exist, the object does not represent a list. ")
+            self.header = None
+            self.nodes = nodes
+            self.if_Doubly_linked = if_Doubly_linked
+            self.size = 0  # size include header node
+        elif nodes[0].if_head!=True:
+            print("error: first node is not header, linked list creates UNsuccessfully !")
+        else:
+            self.header = nodes[0]
+            self.nodes = nodes
+            self.if_Doubly_linked = if_Doubly_linked
+            self.size = 1  # size include header node
+            for i in range(0,len(nodes)-1):
+                nodes[i].next_node = nodes[i+1]
+                self.size += 1
+            if self.if_Doubly_linked == True:
+                for i in range(1,len(nodes)):
+                    nodes[i].pre_node = nodes[i-1]
 
     def get_size(self):
-        return self.size - 1
+        if self.size ==0:
+            print("list does not exist ! return ture")
+            return self.size
+        return self.size - 1  # not count header node
 
     def ifEmpty(self):
-        if self.size ==1:
+        if self.size ==0:
+            print("list does not exist ! return ture")
+            return True
+        elif self.size ==1:
             return True
         else: return False
     
     def first(self):
+        if type(self.header) != type(LL_node()):
+            print("error: the linked list does not exsit, return None value !")
+            return None
+        elif self.header.next_node == None:
+            print("warning: the linked list is empty, return header node !")
+            return self.header
         return self.header.next_node
     
     def last(self):  
+        if self.header==None:
+            print("error: the linked list does not exsit, return None value !")
+            return None
+        elif self.header.next_node == None:
+            print("warning: the linked list is empty, return header node value !")
+            return self.header
         last = self.header
         for i in range(0,self.size):
             if last.next_node == None:
@@ -145,54 +182,98 @@ class linked_list():
             last = last.next_node
     
     def after(self,p):
-        return self.nodes[self.nodes.index(p)].next_node
-    
+        try:
+            return self.nodes[self.nodes.index(p)].next_node
+        except:
+            print('error: node "',p,'" does not exist in current list !')
     def before(self,p):
-        if self.if_Doubly_linked == False:
-            last = self.header
-            for i in range(0,self.size):
-                if last.next_node==p:
-                    return last
-                last = last.next_node
-        else:
-            return self.nodes[self.nodes.index(p)].pre_node
+        try:
+            self.nodes.index(p)
+            if self.if_Doubly_linked == False:
+                last = self.header
+                for i in range(0,self.size):
+                    if last.next_node==p:
+                        return last
+                    last = last.next_node
+            else:
+                return self.nodes[self.nodes.index(p)].pre_node
+        except:
+            print('error: node "',p,'" does not exist in current list !')
     
     def insertAfter(self,p,e):
-        e.next_node = self.nodes[self.nodes.index(p)].next_node
-        self.nodes[self.nodes.index(p)].next_node = e
-        if if_Doubly_linked == True:
-            e.pre_node = self.nodes[self.nodes.index(p)].next_node.pre_node
-            self.nodes[self.nodes.index(p)].next_node.pre_node = e
-        self.size += 1
-    
+        try:
+            e.next_node = self.nodes[self.nodes.index(p)].next_node
+            self.nodes[self.nodes.index(p)].next_node = e
+            if self.if_Doubly_linked == True:
+                e.pre_node = self.nodes[self.nodes.index(p)].next_node.pre_node
+                self.nodes[self.nodes.index(p)].next_node.pre_node = e
+            self.size += 1
+            self.nodes.append(e)
+        except:
+            print('error: node "',p,'" does not exist in current list !')
+
     def insertBefore(self,p,e):
-        last = self.header
-        for i in range(0,self.size):
-            if last.next_node==p:
-                e.next_node = last.next_node
-                last.next_node = e
-                break
-            last = last.next_node
-        if self.if_Doubly_linked == True:
-            e.pre_node = self.nodes[self.nodes.index(p)].pre_node
-            self.nodes[self.nodes.index(p)].pre_node = e
-        self.size += 1
+        try:
+            self.nodes.index(p)
+            if p ==self.header:
+                print("error: cannot insert new node before the header node !")
+            else:
+                last = self.header
+                for i in range(0,self.size):
+                    if last.next_node==p:
+                        e.next_node = last.next_node
+                        last.next_node = e
+                        break
+                    last = last.next_node
+                if self.if_Doubly_linked == True:
+                    e.pre_node = self.nodes[self.nodes.index(p)].pre_node
+                    self.nodes[self.nodes.index(p)].pre_node = e
+                self.size += 1
+                self.nodes.append(p)
+        except:
+            print('error: node "',p,'" does not exist in current list !')
+
 
     def remove(self,p):
-        if if_Doubly_linked == False:
-            start = header
-            for i in range(0,self.size):
-                if start.next_node == p:
-                    start.next_node = p.next_node
+        try:
+            self.nodes.index(p)
+            if p == self.header and self.size > 1:
+                print("error: cannot remove header when list is not empty")
+            
+            elif self.size == 1:
+                self.header = None
+                self.size -= 1
+                self.nodes.remove(p)
+            else:    
+                if self.if_Doubly_linked == False:
+                    start = self.header
+                    for i in range(0,self.size):
+                        if start.next_node == p:
+                            start.next_node = p.next_node
+                            p.next_node = None
+                else:
+                    p.pre_node.next_node = p.next_node
+                    p.next_node.pre_node = p.pre_node
                     p.next_node = None
-        else:
-            p.pre_node.next_node = p.next_node
-            p.next_node.pre_node = p.pre_node
-            p.next_node = None
-            p.pre_node = None
-        self.size -= 1
+                    p.pre_node = None
+                self.size -= 1
+                self.nodes.remove(p)
+            if self.size == 0:
+                self.get_size()
+        except:
+            print('error: node "',p,'" does not exist in current list !')
         
-
+    def get_nodes_data(self):
+        if self.size!=0:
+            start = self.header
+            to_return = ["header"]
+            for i in range(0,self.size):
+                start = start.next_node
+                if start == None:
+                    break
+                to_return.append(start.data)
+        return to_return
+                
 
 # test...:
 
@@ -213,8 +294,10 @@ list1_node2 = LL_node(data = 2)
 list1_node3 = LL_node(data = 3)
 list1_node4 = LL_node(data = 4)
 
-list1 = linked_list(nodes = [list1_header,list1_node1,list1_node2,list1_node3])
-list1.insertBefore(list1_node1,list1_node4)
-print(list1.get_size())
-print(list1.first().data)
-print(list1.last().data)
+list1 = linked_list(nodes = [list1_header,list1_node1])
+
+list1.insertBefore(list1_node1,list1_node2)
+print(list1.get_nodes_data())
+
+#print(list1.after(list1_node4).data,"hree")
+
